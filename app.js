1fb597d6adb2ecd17593c87ce391cfc4f2243e16
Jared <jared_glenn@yahoo.com>
2024-05-28 16:54:37 -0600

    correct corsOptions

--- app.js
+++ app.js
@@ -18,7 +18,7 @@ const morgan = require("morgan");
 const app = express();
 
 const corsOptions = {
-  origin: ['http://localhost:3000', 'http://localhost:3001', 'http://jobly-frontend-rpi2.onrender.com'],
+  origin: ['http://localhost:3000', 'http://localhost:3001', 'https://jobly-frontend-rpi2.onrender.com'],
   methods: ['GET', 'POST', 'PUT', 'DELETE'],
   allowedHeaders: ['Content-Type', 'Authorization']
 };
@@ -43,7 +43,7 @@ app.use(function (req, res, next) {
 app.use(function (err, req, res, next) {
   if (process.env.NODE_ENV !== "test") console.error(err.stack);
   const status = err.status || 500;
-  const message = err.message;
+  const message = err.message || "Something went wrong";
 
   return res.status(status).json({
     error: { message, status },
